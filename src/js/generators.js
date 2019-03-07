2c77203391cdfa1e7857b4b296edce26dc61f244
Krylya <krylya@inbox.ru>
2019-03-07 22:21:05 +0300

    generateTeam

--- src/js/generators.js
+++ src/js/generators.js
@@ -11,4 +11,21 @@ export function* characterGenerator(allowedTypes, maxLevel) {
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  let characterArray = [];
+  let allowedTypesLength = allowedTypes.length; //длина массива
+
+  for(let i = 0; i < characterCount; i+=1) {
+  	// случайное число из длины массива
+  	let randomTypeNum = Math.floor(Math.random() * (allowedTypesLength + 1));
+  	// случайное число из maxLevel
+  	let randomLevel = Math.floor(Math.random() * (maxLevel + 1));
+
+  	// случайный элемент массива
+  	let randomType = allowedTypes[randomTypeNum];
+
+  	// случайный персонаж
+  	let randomCharacter = new randomType(randomLevel);
+  	let character.push(randomCharacter);
+  }
+  return characterArray;
 }
